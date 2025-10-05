b4ffc2dce5b8f91bcfc16f5e3348d5bfad6aa2e6
Damir.? <vip.ushkempir@mail.ru>
2025-10-05 15:24:21 +0500

    init: maven project structure, junit5 setup Additionally, gitignore

--- src/test/java/algorithms/MinHeapTest.java
+++ src/test/java/algorithms/MinHeapTest.java
@@ -1,4 +1,12 @@
-package test.java.algorithms;
+package com.heap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MinHeapTest {
+    @Test
+    void placeholder() {
+        assertTrue(true);
+    }
 }
